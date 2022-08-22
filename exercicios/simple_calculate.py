20243020aacc76ecfd390f5f64b196b967fbf752
unknown <maria.delmonaco@edu.unifil.br>
2022-08-22 17:07:01 -0300

    adicionando exercicios

---
+++ exercicios/simple_calculate.py
@@ -0,0 +1,12 @@
+code1  = int(input())
+units1 = int(input()) 
+price1 = float(input()) 
+
+code2 = int(input())
+units2 = int(input()) 
+price2 = float(input()) 
+
+value = (units1 * price1) + (units2 * price2) 
+
+print(f"VALOR A PAGAR: R$ {value:.2f}")
+
